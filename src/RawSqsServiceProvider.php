9e0a01294cdfa053cff8a764e7b0363c8ee82fae
Usman Shaukat <usmanpakistan@gmail.com>
2025-09-19 01:24:22 +0500

    Update RawSqsServiceProvider.php

--- src/RawSqsServiceProvider.php
+++ src/RawSqsServiceProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace AgentSoftware\LaravelRawSqsConnector;
+namespace UsmanPakistan\LaravelRawSqsConnector;
 
 use Illuminate\Queue\QueueManager;
 use Illuminate\Support\ServiceProvider;
